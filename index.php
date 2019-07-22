573cadf25ebd8155951d7a68b6dc7b1b7c74fffd
mhmmdimamsufiazi <52367102+mhmmdimamsufiazi@users.noreply.github.com>
2019-07-22 13:23:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
             $date = date("Y-m-d");
             // Insert data
             $sql_insert = "INSERT INTO Registration (name, email, job, date) 
-                        VALUES (name, email, job, date)";
+                        VALUES ($name, $email, $job, $date)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
             $stmt->bindValue(2, $email);
